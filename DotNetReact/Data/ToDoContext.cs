cd2e3ae948d9af9e49c3dee8e9a55e0d276a15b3
amir sediqi <amir.sediqi69@gmail.com>
2023-09-07 11:28:30 +0200

    Add project files.

---
+++ DotNetReact/Data/ToDoContext.cs
@@ -0,0 +1,15 @@
+﻿using DotNetReact.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetReact.Data
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Notes> Notes { get; set; }
+    }
+}
---
+++ DotNetReact/Models/Notes.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetReact.Models
+{
+    public class Notes
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
